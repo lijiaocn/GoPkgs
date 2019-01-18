1d8a1873b81139b2a63220c3dfb52e3ceb7d7020
lijiaocn <lijiaocn@foxmail.com>
2019-01-18 16:21:05 +0800

    format

--- virtio/virtio.go
+++ virtio/virtio.go
@@ -1,13 +1,13 @@
-//create: 2015/09/18 11:12:13 change: 2015/09/18 13:51:03 author:lijiao
+//create: 2015/09/18 11:12:13 Change: 2019/01/18 16:20:58 author:lijiao
 package virtio
 
-import(
+import (
 	"io"
 	"sync"
 	"time"
 )
 
-type VirtReader struct{
+type VirtReader struct {
 	mu       sync.Mutex
 	Index    int
 	Capaticy int
@@ -15,18 +15,17 @@ type VirtReader struct{
 	Delay    time.Duration
 }
 
-func (r *VirtReader) Read(p []byte)(n int, err error){
+func (r *VirtReader) Read(p []byte) (n int, err error) {
 	time.Sleep(r.Delay)
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	if r.Index < r. Capaticy - r.Step {
+	if r.Index < r.Capaticy-r.Step {
 		r.Index = r.Index + r.Step
 		return r.Step, nil
-	}else if r.Index >= r.Capaticy{
+	} else if r.Index >= r.Capaticy {
 		return 0, io.EOF
 	}
 	n = r.Capaticy - r.Index
 	r.Index = r.Capaticy
-	return  n, io.EOF
+	return n, io.EOF
 }
-
