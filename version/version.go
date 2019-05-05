acbd484d2e01a928b8e407c62d93e11554364b5f
lijiaocn <lijiaocn@foxmail.com>
2019-05-05 18:07:13 +0800

    just for test

--- version/version.go
+++ version/version.go
@@ -1,15 +1,19 @@
-//create: 2015/05/14 15:55:20 change: 2015/09/16 15:08:57 author:lijiao
+//create: 2015/05/14 15:55:20 Change: 2019/05/05 18:06:58 author:lijiao
 package version
 
-import(
+import (
 	"fmt"
 )
 
-var(
+var (
 	VERSION string
 	COMPILE string
 )
 
-func Show(){
+func Show() {
 	fmt.Printf("version: %s   compile at: %s\n", VERSION, COMPILE)
+}
+
+func Show2() {
+	fmt.Printf("version: %s   compile at: %s\n ( just for test)", VERSION, COMPILE)
 }
