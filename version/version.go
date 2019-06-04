ac07995454df1c45ac11828803d4dbce46e0c96f
lijiaocn <lijiaocn@foxmail.com>
2019-06-04 18:54:23 +0800

    v2 test

--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
-//create: 2015/05/14 15:55:20 Change: 2019/06/04 18:43:36 author:lijiao
+//create: 2015/05/14 15:55:20 Change: 2019/06/04 18:54:10 author:lijiao
 package version
 
 import (
@@ -11,5 +11,5 @@ var (
 )
 
 func Show() {
-	fmt.Printf("version: %s   compile at: %s  golib v1\n", VERSION, COMPILE)
+	fmt.Printf("version: %s   compile at: %s  golib v2\n", VERSION, COMPILE)
 }
